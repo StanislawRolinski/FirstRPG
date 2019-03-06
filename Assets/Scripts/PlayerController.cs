afd9be0eac99ea98db1c886efee40acfa0336067
StanislawRolinski <stasrolinskyi@gmail.com>
2019-03-06 21:01:09 +0100

    0.0.0  start working

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float movementSpeed = 10f;
+
+  
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+        float moveX = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
+        float moveY = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
+
+        transform.Translate(moveX, moveY, 0f);
+    }
+}
